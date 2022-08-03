a0d9a1259a7585361deaae6d2d977621b5070c56
김지우 <jwkim02@miridih.com>
2022-08-03 09:05:41 +0900

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import slido_logo from '/icons/ic_logo.svg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <img src="/icons/ic_logo.svg" className="App-logo" alt="logo"/>
       </header>
+      <main>
+        <div>
+          <h1>Your go-to interaction app for hybrid meetings</h1>
+          <p>Engage your participants with live polls, Q&amp;A, quizzes and word clouds — whether you meet in the office, online or in-between.</p>
+        </div>
+      </main>
     </div>
   );
 }
